82a5b8b3a139ee313c6722b6a63638ee2f6ebb77
sreehariniR <harinisree.ramesh@gmail.com>
2026-04-17 21:34:24 +0530

    Sturcture change

--- Faculty/src/main/java/org/example/faculty/model/LoginModel.java
+++
@@ -1,9 +0,0 @@
-package org.example.faculty.model;
-
-import lombok.Data;
-
-@Data
-public class LoginModel {
-    private String email;
-    private String password;
-}
--- Faculty/src/main/java/org/example/faculty/repository/FacultyRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.faculty.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.example.faculty.model.FacultyModel;
-public interface FacultyRepository extends MongoRepository<FacultyModel,String> {
-    FacultyModel findByEmail(String email);
-
-    boolean existsByEmail(String email);
-}

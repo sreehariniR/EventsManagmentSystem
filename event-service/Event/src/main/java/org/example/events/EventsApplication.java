912657a4a77ddb9c40d02323d11af5651e76c5ed
SivasankariM <sivasankarim1708@gmail.com>
2026-04-18 19:32:27 +0530

    CORS-change

--- event-service/Event/src/main/java/org/example/events/EventsApplication.java
+++ event-service/Event/src/main/java/org/example/events/EventsApplication.java
@@ -1,7 +1,11 @@
 package org.example.events;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EventsApplication {
@@ -9,4 +13,15 @@ public class EventsApplication {
         SpringApplication.run(EventsApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://events-managment-system-teti.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
--- student-service/Student/src/main/java/org/example/student/StudentApplication.java
+++ student-service/Student/src/main/java/org/example/student/StudentApplication.java
@@ -1,7 +1,11 @@
 package org.example.student;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StudentApplication {
@@ -10,4 +14,15 @@ public class StudentApplication {
         SpringApplication.run(StudentApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://events-managment-system-teti.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
